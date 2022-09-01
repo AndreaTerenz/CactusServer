1b391c4b9efdb74234af5106f60a580847144ce3
AndreaTerenz <andrea.terenziani.at@gmail.com>
2022-09-01 11:57:17 +0200

    Basic API test code
    installed gunicorn

--- app.py
+++ app.py
@@ -1,12 +1,22 @@
 from flask import Flask
+import random
 
 app = Flask(__name__)
 
 
-
 @app.route('/')
 def hello_world():  # put application's code here
-    return {'Hello': 'World!'}
+    return {'Hello': random.randint(0, 9)}
+
+
+@app.route('/deck', methods=['GET'])
+def get_deck():  # put application's code here
+    deck = []
+    for i in range(1, 41):
+        deck.append(i % 10)
+    random.shuffle(deck)
+
+    return {'deck': deck}
 
 
 if __name__ == '__main__':
