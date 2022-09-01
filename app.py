b2f4da9b277affb2a5a560e632965196d4989790
AndreaTerenz <andrea.terenziani.at@gmail.com>
2022-09-01 16:40:02 +0200

    leave_lobby route

--- app.py
+++ app.py
@@ -1,6 +1,9 @@
 from flask import Flask, request, json
 import random
 import src.lobby as lobby
+import logging
+
+logging.basicConfig(level=logging.INFO)
 
 app = Flask(__name__)
 
@@ -14,11 +17,29 @@ def hello_world():
 def create_lobby(player_name):
     """Used to create a lobby"""
     l_id, l_idx = lobby.new_lobby(player_name)
+
+    logging.info(f"Created lobby '{l_id}' by player '{player_name}'")
+
     return json.dumps({
         'lobby_id': l_id,
         'lobby_idx': l_idx,
         'success': True,
     }), 200, {'ContentType': 'application/json'}
+
+
+@app.route('/leave-lobby/<player_name>/<lobby_id>', methods=['GET'])
+def leave_lobby(player_name, lobby_id):
+    """Used to create a lobby"""
+    idx = lobby.find_lobby_idx(lobby_id)
+    left, removed = lobby.leave_lobby(player_name, idx)
+
+    if left:
+        logging.info(f"Player '{player_name}' left lobby '{lobby_id}'")
+
+    if removed:
+        logging.info(f"Lobby '{lobby_id}' was removed")
+
+    return json.dumps({'success': left}), 200 if left else 400, {'ContentType': 'application/json'}
 
 
 @app.route('/deck', methods=['GET'])
--- src/lobby.py
+++ src/lobby.py
@@ -1,21 +1,27 @@
 import random
-from dataclasses import dataclass, field
 
 from hashids import Hashids
 
 
 class Lobby:
-    def __init__(self, i: str):
-        self.id: str = i
+    def __init__(self, l_id: str, l_idx: int):
+        self.id: str = l_id
+        self.idx: int = l_idx
         self.players: list[str] = []
 
     def add_player(self, p_id: str):
-        if not(p_id in self.players):
+        if not (p_id in self.players):
             self.players.append(p_id)
+            return True
+
+        return False
 
     def remove_player(self, p_id: str):
-        if (idx := self.find_player(p_id)) in len(self.players):
+        if (idx := self.find_player(p_id)) in range(len(self.players)):
             self.remove_player_at(idx)
+            return True
+
+        return False
 
     def remove_player_at(self, idx: int):
         self.players.pop(idx)
@@ -30,12 +36,10 @@ class Lobby:
     def is_empty(self):
         return len(self.players) <= 0
 
-hashids = Hashids(min_length=6)
 
+hashids = Hashids(min_length=8, salt="rise and shine Dr Freeman")
 lobbies = []
-start_id_idx = random.randint(0, 100)
-current_id_idx = start_id_idx
-print(start_id_idx)
+start_idx = random.randint(0, 100)
 
 
 def new_lobby(creator_id: str):
@@ -43,15 +47,15 @@ def new_lobby(creator_id: str):
     - game settings
     - max players
     """
-    global current_id_idx
+    current_idx = start_idx + len(lobbies)
 
-    lobby_id = hashids.encode(current_id_idx)
-    current_id_idx += 1
+    lobby_id = hashids.encode(current_idx)
 
-    l = Lobby(lobby_id)
+    l = Lobby(lobby_id, current_idx)
     l.add_player(creator_id)
 
     lobbies.append(l)
+
     return lobby_id, len(lobbies) - 1
 
 
@@ -61,9 +65,29 @@ def join_lobby(player_id: str, lobby_idx: int):
 
 
 def leave_lobby(player_id: str, lobby_idx: int):
-    if lobby_idx in range(len(lobbies)):
-        l : Lobby = lobbies[lobby_idx]
-        l.remove_player(player_id)
+    l: Lobby = lobbies[int(lobby_idx)]
 
-        if l.is_empty():
-            lobbies.pop(lobby_idx)
+    left: bool = l.remove_player(player_id)
+
+    removed: bool = False
+    if l.is_empty():
+        lobbies.pop(lobby_idx)
+        removed = True
+
+    return left, removed
+
+def lobby_id_range():
+    return range(start_idx + len(lobbies))
+
+def find_lobby(lobby_id: str):
+    for l in lobbies:
+        if l.id == lobby_id:
+            return l
+
+    return None
+def find_lobby_idx(lobby_id: str):
+    for idx, l in enumerate(lobbies):
+        if l.id == lobby_id:
+            return idx
+
+    return -1
